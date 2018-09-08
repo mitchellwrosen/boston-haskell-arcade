f04e665e26985f4fcb83a0b1e085478d17b096b3
Mitchell Rosen <mitchellwrosen@gmail.com>
2018-09-08 15:32:24 -0500

    Nicer looking easel, erase with backspace

--- src/Bha/Game/Impl/Paint.hs
+++ src/Bha/Game/Impl/Paint.hs
@@ -15,7 +15,7 @@ type Row = Int
 
 ccol    =  8 :: Int -- canvas top-left col
 crow    =  4 :: Int -- canvas top-left row
-cwidth  = 42 :: Int -- canvas width
+cwidth  = 50 :: Int -- canvas width
 cheight = 20 :: Int -- canvas height
 
 data Color
@@ -26,6 +26,7 @@ data Color
   | Magenta
   | Yellow
   | Cyan
+  | Black
   deriving (Bounded, Enum, Eq)
 
 moment
@@ -33,21 +34,27 @@ moment
   -> Banana (Behavior Scene, Events ())
 moment eEvent = mdo
   let
-    eUp    = filterE (== EventKey KeyArrowUp    False) eEvent
-    eDown  = filterE (== EventKey KeyArrowDown  False) eEvent
-    eLeft  = filterE (== EventKey KeyArrowLeft  False) eEvent
-    eRight = filterE (== EventKey KeyArrowRight False) eEvent
-    eEsc   = filterE (== EventKey KeyEsc        False) eEvent
-    eEnter = filterE (== EventKey KeyEnter      False) eEvent
-    eTab   = filterE (== EventKey KeyTab        False) eEvent
-    eH     = filterE (== EventKey (KeyChar 'h') False) eEvent
-    eJ     = filterE (== EventKey (KeyChar 'j') False) eEvent
-    eK     = filterE (== EventKey (KeyChar 'k') False) eEvent
-    eL     = filterE (== EventKey (KeyChar 'l') False) eEvent
+    eUp        = filterE (== EventKey KeyArrowUp    False) eEvent
+    eDown      = filterE (== EventKey KeyArrowDown  False) eEvent
+    eLeft      = filterE (== EventKey KeyArrowLeft  False) eEvent
+    eRight     = filterE (== EventKey KeyArrowRight False) eEvent
+    eEsc       = filterE (== EventKey KeyEsc        False) eEvent
+    eEnter     = filterE (== EventKey KeyEnter      False) eEvent
+    eSpace     = filterE (== EventKey KeySpace      False) eEvent
+    eBackspace = filterE (== EventKey KeyBackspace2 False) eEvent
+    eTab       = filterE (== EventKey KeyTab        False) eEvent
+    eH         = filterE (== EventKey (KeyChar 'h') False) eEvent
+    eJ         = filterE (== EventKey (KeyChar 'j') False) eEvent
+    eK         = filterE (== EventKey (KeyChar 'k') False) eEvent
+    eL         = filterE (== EventKey (KeyChar 'l') False) eEvent
+
+  bBackground :: Behavior Bool <-
+    accumB True (not <$ eSpace)
 
   bCanvas :: Behavior (HashMap (Col, Row) Color) <-
     accumB mempty $ unions
       [ HashMap.insert <$> bCursor <*> bColor <@ eEnter
+      , HashMap.delete <$> bCursor <@ eBackspace
       ]
 
   bColor :: Behavior Color <-
@@ -68,7 +75,7 @@ moment eEvent = mdo
   let
     bCells :: Behavior Cells
     bCells =
-      render <$> bCanvas <*> bCursor <*> bColor
+      render <$> bBackground <*> bCanvas <*> bCursor <*> bColor
 
   let
     bScene :: Behavior Scene
@@ -85,11 +92,11 @@ cycleSelected =
  where
   xs = cycle [minBound..maxBound] :: [Color]
 
-render :: HashMap (Col, Row) Color -> (Col, Row) -> Color -> Cells
-render canvas cursor selected =
+render :: Bool -> HashMap (Col, Row) Color -> (Col, Row) -> Color -> Cells
+render background canvas cursor selected =
   mconcat
     [ renderCanvasBorder
-    , renderCanvas canvas
+    , renderCanvas background canvas
     , renderCursor cursor selected
     , renderEasel
     , renderSelected selected
@@ -104,9 +111,21 @@ renderCanvasBorder =
     , rect (ccol+cwidth+2) (crow-1)         2          (cheight+2) yellow
     ]
 
-renderCanvas :: HashMap (Col, Row) Color -> Cells
-renderCanvas =
-  foldMap (\((c, r), x) -> set c r (Cell ' ' mempty (attr x))) . HashMap.toList
+renderCanvas :: Bool -> HashMap (Col, Row) Color -> Cells
+renderCanvas background cells =
+  mconcat
+    [ if background
+        then
+          foldMap
+            (\(c, r) -> set c r (Cell '‧' mempty mempty))
+            ((,) <$> [ccol..ccol+cwidth-1] <*> [crow..crow+cheight-1])
+        else
+          mempty
+
+    , foldMap
+        (\((c, r), x) -> set c r (Cell ' ' mempty (attr x)))
+        (HashMap.toList cells)
+    ]
 
 renderCursor :: (Col, Row) -> Color -> Cells
 renderCursor (c, r) color =
@@ -114,20 +133,30 @@ renderCursor (c, r) color =
 
 renderEasel :: Cells
 renderEasel =
-  foldMap
-    (\(i, c) ->
-      let
-        w = 6 :: Int
-        h = 3 :: Int
-      in
-        rect (ccol+i*w) (crow+cheight+h) w h (attr c))
-    (zip [0..] [minBound..maxBound])
+  mconcat
+    [ foldMap (\r -> set ccol (crow+cheight+r) (Cell '│' white black)) [4..7]
+    , foldMap (\r -> set (ccol+cwidth) (crow+cheight+r) (Cell '│' white black)) [4..7]
+    , foldMap (\c -> set (ccol+c+1) (crow+cheight+3) (Cell '─' white black)) [0..cwidth-2]
+    , foldMap (\c -> set (ccol+c+1) (crow+cheight+8) (Cell '─' white black)) [0..cwidth-2]
+    , set ccol (crow+cheight+3) (Cell '┌' white black)
+    , set ccol (crow+cheight+8) (Cell '└' white black)
+    , set (ccol+cwidth) (crow+cheight+3) (Cell '┐' white black)
+    , set (ccol+cwidth) (crow+cheight+8) (Cell '┘' white black)
+    , foldMap
+        (\(i, c) ->
+          let
+            w = 6 :: Int
+            h = 3 :: Int
+          in
+            rect (ccol+1+i*w) (crow+cheight+h+1) w h (attr c))
+        (zip [0..] [minBound..maxBound])
+    ]
 
 renderSelected :: Color -> Cells
 renderSelected color =
   text
-    (ccol + fromEnum color * 6 + 2)
-    (crow+cheight+6)
+    (ccol + fromEnum color * 6 + 3)
+    (crow+cheight+7)
     mempty
     mempty
     "••"
@@ -141,3 +170,4 @@ attr = \case
   Magenta -> magenta
   Yellow  -> yellow
   Cyan    -> cyan
+  Black   -> black
